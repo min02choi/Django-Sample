45547134bbfe30b3862f424fceec5bfdb5f070e9
[min02choi] <min02choi@naver.com>
2023-04-30 18:37:34 +0900

    [DT:2023-04-30] Django 2-2

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,27 @@
-from django.db import models
+import datetime
 
-# Create your models here.
+from django.db import models
+from django.utils import timezone
+
+
+# Question 테이블
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+
+# Choice 테이블
+# 하나의 Quesion table안에 여러 개의 Choice가 있음 (1:N 관계)
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
